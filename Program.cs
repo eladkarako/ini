3d9a366be4faba46bf47844a205dbabc1255c31e
Elad Karako <eladkarako@users.noreply.github.com>
2018-01-24 17:48:29 +0200

    initial (read values, example)

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace iniRun
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string filename = @".\sample.ini";
+      /*sample.ini:
+       * [foods]
+       * pizza=margherita
+       * bread=full grain
+       * 
+       * [drinks]
+       * cola=zero
+       * 7up=diet
+       */
+
+      //should say "food\r\ndrinks"
+      string[] categories = IniHandler.GetCategories(filename);
+      foreach (string category in categories)
+        Console.WriteLine(category);
+
+
+      //should say "pizza\r\nbread"
+      string[] keys = IniHandler.GetKeys(filename, "foods");
+      foreach (string key in keys)
+        Console.WriteLine(key);
+
+      //should say "margherita"
+      string value = IniHandler.GetValue(filename, "foods", "pizza", "vegan");
+      Console.WriteLine(value);
+
+
+    }
+  }
+}
